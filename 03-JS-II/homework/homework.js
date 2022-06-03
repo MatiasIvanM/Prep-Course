b542fa4e79bfd440266204b2542c65a432aeb94f
MatiasIvanM <matiasivan.sd@gmail.com>
2022-06-03 13:08:55 -0300

    modif terimnado sin correccion

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -42,10 +42,10 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  if (idioma === "mandarin"){
-    return "Ni Hao!";
-  }else if (idioma === "aleman"){
+  if (idioma === "aleman"){
     return "Guten Tag!";
+  }else if (idioma === "mandarin"){
+    return "Ni Hao!";
   } else if (idioma === "ingles") {
     return "Hello!";
   } else return "Hola!";
